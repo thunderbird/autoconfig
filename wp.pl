a9466641db26641311727d1050c1a76737d50e2a
Andrew Sutherland <bugmail@asutherland.org>
2016-02-29 22:18:50 +0000

    Bug 1093606 - Missing IMAP autoconfiguration entry for wp.pl. r=asuth
    git-svn-id: https://svn.mozilla.org/mozillamessaging.com/sites/autoconfig.mozillamessaging.com/trunk@150303 4eb1ac78-321c-0410-a911-ec516a8615a5

--- wp.pl
+++ wp.pl
@@ -1,22 +1,34 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <clientConfig version="1.1">
-  <emailProvider id="wp.pl">
-    <domain>wp.pl</domain>
-    <displayName>Poczta Wirtualna Polska</displayName>
-    <displayShortName>Poczta WP</displayShortName>
-    <incomingServer type="pop3">
-      <hostname>pop3.wp.pl</hostname>
-      <port>995</port>
-      <socketType>SSL</socketType>
-      <username>%EMAILLOCALPART%</username>
-      <authentication>password-cleartext</authentication>
-    </incomingServer>
-    <outgoingServer type="smtp">
-      <hostname>smtp.wp.pl</hostname>
-      <port>587</port>
-      <socketType>STARTTLS</socketType>
-      <username>%EMAILLOCALPART%</username>
-      <authentication>password-cleartext</authentication>
-    </outgoingServer>
-  </emailProvider>
+    <emailProvider id="wp.pl">
+      <domain>wp.pl</domain>
+      <displayName>Poczta Wirtualna Polska</displayName>
+      <displayShortName>Poczta WP</displayShortName>
+
+      <incomingServer type="imap">
+         <hostname>imap.wp.pl</hostname>
+         <port>993</port>
+         <socketType>SSL</socketType>
+         <authentication>password-cleartext</authentication>
+         <username>%EMAILLOCALPART%</username>
+      </incomingServer>
+      <incomingServer type="pop3">
+         <hostname>pop3.wp.pl</hostname>
+         <port>995</port>
+         <socketType>SSL</socketType>
+         <authentication>password-cleartext</authentication>
+         <username>%EMAILLOCALPART%</username>
+      </incomingServer>
+
+      <outgoingServer type="smtp">
+         <hostname>smtp.wp.pl</hostname>
+         <port>465</port>
+         <socketType>SSL</socketType>
+         <authentication>password-cleartext</authentication>
+         <username>%EMAILLOCALPART%</username>
+      </outgoingServer>
+	  
+      <documentation url="http://poczta.wp.pl/info-pomoc-konfig.html?action=more&amp;id=157">
+         <descr lang="pl">konfiguracja programów pocztowych (Mozilla Thunderbird)</descr>
+      </documentation>
+    </emailProvider>
 </clientConfig>
