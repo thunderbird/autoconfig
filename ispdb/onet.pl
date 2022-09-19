f5e2cffdc024355e5cf69a123de2a77cee73b1b4
Marcin Mikołajczak <me@mkljczk.pl>
2022-09-19 16:38:24 +0200

    Update onet.pl

--- ispdb/onet.pl
+++ ispdb/onet.pl
@@ -11,8 +11,22 @@
     <domain>buziaczek.pl</domain>
     <domain>amorki.pl</domain>
     <domain>republika.pl</domain>
-    <displayName>Poczta Onet</displayName>
+    <domain>adres.pl</domain>
+    <domain>cyberia.pl</domain>
+    <domain>onet.com.pl</domain>
+    <domain>opoczta.pl</domain>
+    <domain>pseudonim.pl</domain>
+    <domain>spoko.pl</domain>
+    <domain>vip.onet.pl</domain>
+    <displayName>Onet Poczta</displayName>
     <displayShortName>Onet</displayShortName>
+    <incomingServer type="imap">
+      <hostname>imap.poczta.onet.pl</hostname>
+      <port>993</port>
+      <socketType>SSL</socketType>
+      <username>%EMAILADDRESS%</username>
+      <authentication>password-cleartext</authentication>
+    </incomingServer>
     <incomingServer type="pop3">
       <hostname>pop3.poczta.onet.pl</hostname>
       <port>995</port>
@@ -34,5 +48,8 @@
       <username>%EMAILADDRESS%</username>
       <authentication>password-cleartext</authentication>
     </outgoingServer>
+    <documentation url="https://pomoc.poczta.onet.pl/baza-wiedzy/konfiguracja-poczty-w-programie-mozilla-thunderbird/">
+      <descr lang="pl">Konfiguracja poczty: Mozilla Thunderbird</descr>
+    </documentation>
   </emailProvider>
 </clientConfig>
